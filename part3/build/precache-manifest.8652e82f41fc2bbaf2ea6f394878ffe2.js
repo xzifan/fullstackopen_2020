85dfe2a5ae1bf1f9d35aaec2e319d76151a7c290
xzifan <xiao@zifan.tech>
2020-12-17 13:40:33 +0200

    part3 phonebook fullstack

---
+++ part3/build/precache-manifest.8652e82f41fc2bbaf2ea6f394878ffe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64433549bc027640f33f196b51c891f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f932c2c2cc466afcdfbf",
+    "url": "/static/css/main.c37edac4.chunk.css"
+  },
+  {
+    "revision": "eb350b07a9b7c34a84f1",
+    "url": "/static/js/2.c0dfe6dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c0dfe6dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f932c2c2cc466afcdfbf",
+    "url": "/static/js/main.52a88b5a.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- part3/index.js
+++ part3/index.js
@@ -3,80 +3,97 @@ const express = require('express')
 const { format } = require('morgan')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
-morgan.token('body',(req,res)=>{
-    return JSON.stringify(req.body)
+app.use(cors())
+app.use(express.static('build'))
+morgan.token('body', (req, res) => {
+	return JSON.stringify(req.body)
 })
-app.use(morgan({format:':method :url :status :res[content-length] - :response-time ms :body'}))
-let data = {
-    "persons":[
-      { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-      },
-      { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-      },
-      { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-      }
-    ]
-  }
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+var persons = [
+	{
+		"name": "Arto Hellas",
+		"number": "040-123456",
+		"id": 1
+	},
+	{
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523",
+		"id": 2
+	},
+	{
+		"name": "Dan Abramov",
+		"number": "12-43-234345",
+		"id": 3
+	},
+	{
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122",
+		"id": 4
+	}
+]
+
 
 app.get('/info', (request, response) => {
-    const datetime = new Date()
-    const text = datetime.toLocaleString('en-us', { dateStyle:"long", weekday: 'short', timeStyle:"full",month:"short" , timeZone:"Europe/Helsinki",timeZoneName:"long", day:"2-digit",hour:"2-digit", minute:"2-digit",second:"2-digit",houryear:"numeric",timeZoneName:"long"}) 
-    response.send('Phonebook has info for '+data.persons.length+' people</br>'+text)
+	const datetime = new Date()
+	const text = datetime.toLocaleString('en-us', { dateStyle: "long", weekday: 'short', timeStyle: "full", month: "short", timeZone: "Europe/Helsinki", timeZoneName: "long", day: "2-digit", hour: "2-digit", minute: "2-digit", second: "2-digit", houryear: "numeric", timeZoneName: "long" })
+	response.send('Phonebook has info for ' + persons.length + ' people</br>' + text)
 })
-  
+
 app.get('/api/persons', (request, response) => {
-    response.contentType('json')
-    response.json(data)
+	response.contentType('json')
+	response.json(persons)
 })
 
-app.get('/api/persons/:id',(request, response) => {
-    const id = Number(request.params.id)
-    const match = data.persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const match = persons.find(person => person.id === id)
 
-    if (match)
-        response.json(match)
-    else
-        response.status(404).end("Person not found")
+	if (match)
+		response.json(match)
+	else
+		response.status(404).send("Person not found")
 })
 
-app.delete('/api/persons/:id',(request, response) => {
-    const id = Number(request.params.id)
-    data.persons = data.persons.filter(person => person.id ===id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	if (!persons.find(person => person.id === id))
+		response.status(404).end("Person not found")	
+	persons = persons.filter(person => person.id !== id)
+	response.status(204).send("Contact removed")
 })
 
-app.post('/api/persons',(request, response)=>{
-    console.log(request.body.name)
-    if (!request.body.name || !request.body.number)
-        response.status(400).send({ error: "name or number missing "})
-    else if (data.persons.find(person => person.name === request.body.name))
-        response.status(403).send({ error: 'name must be unique' })
-    else {
-        var newPerson = {...request.body,id:Math.floor(Math.random()*1000000)}
-        data.persons.push(newPerson)
-        console.log(data)
-        response.status(201).end()
-    }
+app.post('/api/persons', (request, response) => {
+	if (!request.body.name || !request.body.number)
+		response.status(400).send({ error: "name or number missing " })
+	else if (persons.find(person => person.name === request.body.name))
+		response.status(403).send({ error: 'name must be unique' })
+	else {
+		var newPerson = { ...request.body, id: Math.floor(Math.random() * 1000000) }
+		persons.push(newPerson)
+		response.status(201).send("Contact added!")
+	}
 })
 
-const PORT = 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+app.put('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const targetIdx = persons.findIndex(person => person.id ===id)
+	if (!persons.find(person => person.id === id))
+		response.status(404).send({error:persons.find(person => person.id === id)})
+	else if (!request.body.name || !request.body.number)
+		response.status(400).send({ error: "name or number missing " })
+	else if (persons.find(person => person.name === request.body.name) && persons.findIndex(person => person.name === request.body.name) !== targetIdx)
+		response.status(403).send({ error: 'name must be unique' })
+	else {
+		persons[targetIdx].name = request.body.name
+		persons[targetIdx].number = request.body.number
+		response.status(201).send("contact updated! ID:"+id)
+	}
+})
+
+app.listen(process.env.PORT||3001, () => {
+	console.log('Server running on port ' +(process.env.PORT || "3001"))
 })
