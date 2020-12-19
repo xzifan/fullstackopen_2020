2e9f57f7b918a8e7f8ca724d20fa7c476eb7396f
xzifan <xiao@zifan.tech>
2020-12-19 13:11:26 +0200

    frontend adjustments, rebuild

--- part3/build/precache-manifest.98c710c319a92625a3adbea0f695ff49.js
+++ part3/build/precache-manifest.98c710c319a92625a3adbea0f695ff49.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cee033640ac4a7f0b1ab6ad295e97b16",
+    "revision": "943d45cc1bc869f2ccbf04a1a1cd725f",
     "url": "/index.html"
   },
   {
-    "revision": "2c2999f2cbcf0d242351",
+    "revision": "591cc8345248f0c717d0",
     "url": "/static/css/main.c37edac4.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c0dfe6dc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2c2999f2cbcf0d242351",
-    "url": "/static/js/main.dad6eec5.chunk.js"
+    "revision": "591cc8345248f0c717d0",
+    "url": "/static/js/main.c74fdb62.chunk.js"
   },
   {
     "revision": "f4d2ac87a76b6331fd91",
--- part3/build/service-worker.js
+++ part3/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.570054646db933d299da2d5df5a3a8b1.js"
+  "/precache-manifest.98c710c319a92625a3adbea0f695ff49.js"
 );
 
 self.addEventListener('message', (event) => {
