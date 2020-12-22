6c1970ebf4cfe6f525b874ad6e4b73563aaadb89
xzifan <xiao@zifan.tech>
2020-12-22 04:20:56 +0200

    Exercise 4.6-4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,3 +1,5 @@
+const blog = require('../models/blog')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -18,10 +20,62 @@ const favoriteBlog = (blogs)=>{
         delete favBlog.url
         return favBlog
     } else return null
-    
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length ==0) return null
+    var authors = []
+    var count = []
+    for (let i =0;i<blogs.length;i++){
+        let item = blogs[i]
+        let authorIndex = authors.indexOf(item.author)     
+        if (authorIndex!=-1) count[authorIndex]++
+        else {authors.push(item.author);count.push(1)}
+    }
+    var topCount = 0
+    var name = ''
+    for (let i=0;i<count.length;i++){
+        if (count[i]>topCount){
+            topCount = count[i]
+            name = authors[i]
+        }
+    }
+    var author = {
+        author:name,
+        blogs:topCount
+    }
+    return author
+}
+
+const mostLikes= (blogs)=>{
+    if (blogs.length ==0) return null
+    var authors = []
+    var count = []
+    for (let i =0;i<blogs.length;i++){
+        let item = blogs[i]
+        let authorIndex = authors.indexOf(item.author)     
+        if (authorIndex!=-1) count[authorIndex]+=item.likes
+        else {authors.push(item.author);count.push(item.likes)}
+    }
+    var topCount = 0
+    var name = ''
+    for (let i=0;i<count.length;i++){
+        if (count[i]>topCount){
+            topCount = count[i]
+            name = authors[i]
+        }
+    }
+    var author = {
+        author:name,
+        likes:topCount
+    }
+    return author
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
