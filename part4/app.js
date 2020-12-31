491bb08210d1f6686c25407a91065ff062e554df
xzifan <xiao@zifan.tech>
2020-12-31 18:56:12 +0200

    Changed for testing of part5

--- part4/app.js
+++ part4/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const usersRouter = require('./controllers/users')
 const blogsRouter = require('./controllers/blogs')
 const loginRouter = require('./controllers/login')
+const testingRouter = require('./controllers/testing')
 
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -30,6 +31,11 @@ app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login',loginRouter)
+
+if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+  }  
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
