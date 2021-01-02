b5befbcc702cacdc85a65aeedbbddcb6c1bfbf6f
xzifan <xiao@zifan.tech>
2021-01-02 14:56:28 +0200

    Exercise 6.9

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
-// import { useSelector, useDispatch } from 'react-redux'
-import { createStore } from 'redux'
-import anecdoteReducer from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import store from './store'
 
 const App = () => {
-    const store = createStore(anecdoteReducer)
     return (
         <div>
             <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { voteFor } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = (props)=>{
